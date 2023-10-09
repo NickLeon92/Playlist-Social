dd5dc662ef4b535931738335303a0835caa47223
Nicolas Leon <nicolas7@vt.edu>
2023-10-09 17:42:46 -0400

    title changes

--- vite-project/src/components/Playlist.tsx
+++ vite-project/src/components/Playlist.tsx
@@ -122,8 +122,8 @@ const playlistDivRef = useRef<HTMLDivElement | null>(null);
       <form>
       <div className="relative z-0 w-full mb-6 group flex align center">
         <div className="mr-2 border border-blue-900 border-t-0 border-l-0 border-r-2 border-b-0 pr-2">
-          <input onChange={titleInput} className="text-xl block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" />
-          <label className="truncate text-xl peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{title === ''? ('Title'):(title)}</label>
+          <input onChange={titleInput} className="text-xl block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder={title === ''?('Title'):(title)} />
+          {/* <label className="truncate text-xl peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{title === ''? ('Title'):(title)}</label> */}
           <p id="helper-text-explanation" className="mt-2 text-xs text-gray-500 dark:text-gray-400">Enter a title for you playlist!</p>
         </div>
 
